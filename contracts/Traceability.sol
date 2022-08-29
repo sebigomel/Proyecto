ff62857e9234485bfff8f48fe4e0f1311416e425
Remix Workspace <>
2022-08-29 07:31:57 -0300

    uploadinfo

--- contracts/Traceability.sol
+++ contracts/Traceability.sol
@@ -6,12 +6,37 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Traceability is Ownable {
 
+    struct Product {
+        uint256 ID;
+        bytes32 ProductHash;
+        address EnterpriseEA; 
+    }
+
     mapping(address => bool) public producers;
+    Product[] public products;
+    uint private productCounter;
+
+    event Action (
+        uint256 ID,
+        string actionType,
+        address indexed executor,
+        bool success
+    );
 
     function registerProducer(address producerEA) public onlyOwner{
         producers[producerEA] = true;
     }
 
-    
+    function createNewProduction() external {
+        products.push(Product(productCounter, "0n", msg.sender));
+        productCounter++;
+    }
+
+    function uploadTraceabilityInfo(uint256 ID, bytes32 merkleTreeRoot) external {
+        require(msg.sender == products[ID].EnterpriseEA, "No tienes permiso para alterar este producto");
+        products[ID].ProductHash = merkleTreeRoot;
+        emit Action(ID, "Upload Information", msg.sender, true);
+    }
+ 
 
     }
